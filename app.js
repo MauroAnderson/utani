0780ef2e778b08de1d1aadcc832b81813efbbcfa
MauroAnderson <53186544+MauroAnderson@users.noreply.github.com>
2026-04-20 23:03:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ fetch(url)
       const col = fila.split(",");
 
       const nombre = col[1];
-      const precio = col[2];
-      const imagen = col[3];
+      const precio = col[3];
+      const precioOferta = col[4];
+      const descripcion = col[5];
+      const imagen = col[6];
 
       document.getElementById("productos").innerHTML += `
         <div class="card">
-          <img src="${imagen}">
+          <img src="${imagen}" width="200">
           <h3>${nombre}</h3>
-          <p>S/ ${precio}</p>
+          <p>${descripcion}</p>
+          <p><del>S/ ${precio}</del> <b>S/ ${precioOferta}</b></p>
           <a target="_blank" href="https://wa.me/51999999999?text=Quiero cotizar ${nombre}">
             Cotizar
           </a>
