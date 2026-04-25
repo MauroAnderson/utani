e5f037891affed8fd8de9ee7c3681d81ca2b08e2
MauroAnderson <53186544+MauroAnderson@users.noreply.github.com>
2026-04-25 11:06:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxV_Ae4z_UyHXA5cXtTi9Ap5ZNdHJrpEn7p2Dx07iAJBQH814jw4p6tBslh3fsCZhnTTExdVLPLPLK/pub?output=csv";
 
-
-let productos = [];
+let productos=[];
 
 fetch(url)
 .then(r=>r.text())
 .then(data=>{
-  const filas = data.split("\n").slice(1);
+  const filas=data.split("\n").slice(1);
 
   filas.forEach(f=>{
-    let c = f.split(",");
-    if(c.length < 8) return;
+    let c=f.split(",");
+    if(c.length<8) return;
 
     productos.push({
       cliente:c[0],
@@ -26,7 +25,7 @@ fetch(url)
   iniciar();
 });
 
-/* OBTENER CLIENTE */
+/* CLIENTE */
 function getCliente(){
   let params = new URLSearchParams(window.location.search);
   return params.get("cliente");
@@ -36,20 +35,21 @@ function getCliente(){
 function iniciar(){
   let cliente = getCliente();
 
-  if(cliente){
-    let lista = productos.filter(p =>
-      p.cliente.toLowerCase() === cliente.toLowerCase()
-    );
-
-    document.getElementById("tituloCliente").innerHTML =
-      "🧾 Cotización para <b>"+cliente+"</b>";
-
-    render(lista);
-    total(lista);
-
-  } else {
-    render(productos);
+  if(!cliente){
+    document.getElementById("mensaje").innerHTML =
+      "⚠️ Esta página es una cotización personalizada.<br>Solicita tu link al asesor.";
+    return;
   }
+
+  let lista = productos.filter(p =>
+    p.cliente.toLowerCase() === cliente.toLowerCase()
+  );
+
+  document.getElementById("tituloCliente").innerHTML =
+    "🧾 Cotización para <b>"+cliente+"</b>";
+
+  render(lista);
+  total(lista);
 }
 
 /* RENDER */
@@ -63,16 +63,18 @@ function render(lista){
 
       <div class="nombre">${p.nombre}</div>
 
-      ${
-        p.oferta
-        ? `<div class="precio-old">S/ ${p.precio}</div>
-           <div class="precio">S/ ${p.oferta}</div>`
-        : `<div class="precio">S/ ${p.precio}</div>`
-      }
+      <div class="precio">S/ ${p.oferta || p.precio}</div>
+
+      <button class="btn" onclick="verDesc('${p.descripcion}')">Detalle</button>
+
+      ${p.obsequio ? `
+        <div class="nombre">🎁 ${p.obsequio}</div>
+        <div class="precio">S/ 0</div>
+      ` : ""}
     </div>`;
   });
 
-  document.getElementById("productos").innerHTML = html;
+  document.getElementById("productos").innerHTML=html;
 }
 
 /* TOTAL */
@@ -83,4 +85,14 @@ function total(lista){
 
   document.getElementById("total").innerHTML =
     "💰 Total: S/ " + suma;
+}
+
+/* MODAL */
+function verDesc(texto){
+  document.getElementById("contenidoModal").innerHTML = texto;
+  document.getElementById("modal").style.display="flex";
+}
+
+function cerrarModal(){
+  document.getElementById("modal").style.display="none";
 }
